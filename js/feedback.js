7c9217e5a627fbcae52500e607571513bbc87c72
MerchanskyAlexandr <alexandrmerchansky@gmail.com>
2015-06-09 21:27:25 +0300

    first js commit

---
+++ js/feedback.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function() {
+	var text = "Ваше Ім'я";
+	$(".full-name").focus(function() {
+    $(this).addClass("active");
+    if($(this).attr("value") == text) {
+		$(this).attr("value", "");
+	}
+});
+$(".full-name").blur(function() {
+    $(this).removeClass("active");
+    if($(this).attr("value") == "") {
+		$(this).attr("value", text);
+	}
+});
+});
+$(document).ready(function() {
+	var text = "Ваш e-mail";
+	$(".e-mail").focus(function() {
+    $(this).addClass("active");
+    if($(this).attr("value") == text) {
+		$(this).attr("value", "");
+	}
+});
+$(".e-mail").blur(function() {
+    $(this).removeClass("active");
+    if($(this).attr("value") == "") {
+		$(this).attr("value", text);
+	}
+});
+});
+$(document).ready(function() {
+	$("#request-button").click(function() {
+		alert("Дякую за ваш відгук!")
+	}
+);
+});
---
+++ js/hotels-reserve.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function() {
+	var text = "Ваше Ім'я";
+	$("#first-name").focus(function() {
+    $(this).addClass("active");
+    if($(this).attr("value") == text) {
+		$(this).attr("value", "");
+	}
+});
+$("#first-name").blur(function() {
+    $(this).removeClass("active");
+    if($(this).attr("value") == "") {
+		$(this).attr("value", text);
+	}
+});
+});
+$(document).ready(function() {
+	var text = "Ваше Прізвище";
+	$("#last-name").focus(function() {
+    $(this).addClass("active");
+    if($(this).attr("value") == text) {
+		$(this).attr("value", "");
+	}
+});
+$("#last-name").blur(function() {
+    $(this).removeClass("active");
+    if($(this).attr("value") == "") {
+		$(this).attr("value", text);
+	}
+});
+});
+$(document).ready(function() {
+	var text = "Ваш e-mail";
+	$(".e-mail").focus(function() {
+    $(this).addClass("active");
+    if($(this).attr("value") == text) {
+		$(this).attr("value", "");
+	}
+});
+$(".e-mail").blur(function() {
+    $(this).removeClass("active");
+    if($(this).attr("value") == "") {
+		$(this).attr("value", text);
+	}
+});
+});
+$(document).ready(function() {
+	var text = "Ваш номер телефону";
+	$(".telephone").focus(function() {
+    $(this).addClass("active");
+    if($(this).attr("value") == text) {
+		$(this).attr("value", "");
+	}
+});
+$(".telephone").blur(function() {
+    $(this).removeClass("active");
+    if($(this).attr("value") == "") {
+		$(this).attr("value", text);
+	}
+});
+});
+$(document).ready(function() {
+	$("#request-button").click(function() {
+		alert("Ваш запит на резервацію отримано!")
+	}
+);
+});
